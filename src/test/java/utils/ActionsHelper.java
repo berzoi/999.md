ec6dd08531f842a2f345ad03ece93d1b3e71180c
berzoi <berzoi.lolian@gmail.com>
2023-06-19 10:29:06 +0300

    modified notifications

---
+++ src/test/java/utils/ActionsHelper.java
@@ -0,0 +1,144 @@
+package utils;
+
+import static com.microsoft.playwright.options.WaitForSelectorState.VISIBLE;
+import static com.microsoft.playwright.options.WaitUntilState.DOMCONTENTLOADED;
+
+import com.microsoft.playwright.ElementHandle;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Page.NavigateOptions;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ActionsHelper {
+  private final Page page;
+
+  public ActionsHelper(Page page) {
+    this.page = page;
+  }
+
+//  public static void clickElement(SelenideElement element) {
+//    element.scrollTo().click();
+//  }
+//
+//  public static void getMessage(SelenideElement element, String message) {
+//    element.shouldHave(text(message));
+//  }
+//
+//  public void navigateTo(String url) {
+//    new BaseTestSelenide(url);
+//  }
+
+  public void waitForSelector(String selector){
+    page.waitForSelector(selector, new Page.WaitForSelectorOptions().setState(VISIBLE));
+  }
+
+  public void openLink (String url){
+    page.navigate(url, new NavigateOptions().setWaitUntil(DOMCONTENTLOADED));
+  }
+  public void waiter (int timeToWait){
+    try {
+      Thread.sleep(timeToWait);
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public void getNumberOAttribute() {
+    List<ElementHandle> elements = page.querySelectorAll("[data-test-item-type='standard']");
+    Map<String, Integer> elementCount = new HashMap<>();
+
+    for (ElementHandle element : elements) {
+      String attributeValue = element.getAttribute("data-test-item-state");
+      elementCount.put(attributeValue, elementCount.getOrDefault(attributeValue, 0) + 1);
+    }
+
+    for (Map.Entry<String, Integer> entry : elementCount.entrySet()) {
+      System.out.println(entry.getKey() + ": " + entry.getValue());
+    }
+  }
+
+  //  public void getNumberOAttribute() {
+//
+//    List<ElementHandle> elements = page.querySelectorAll("[data-test-item-type='standard']");
+//    List<String> value = new ArrayList<>();
+//    for (ElementHandle element : elements) {
+//      String attributeValue = element.getAttribute("data-test-item-state");
+//      value.add(attributeValue);
+//    }
+//
+//    Map<String, Integer> elementCount = new HashMap<>();
+//    for (String element : value) {
+//      if (elementCount.containsKey(element)) {
+//        // If the element is already in the map, increment its count by 1
+//        elementCount.put(element, elementCount.get(element) + 1);
+//      } else {
+//        // If the element is not in the map, add it with a count of 1
+//        elementCount.put(element, 1);
+//      }
+//    }
+//
+//    for (Map.Entry<String, Integer> entry : elementCount.entrySet()) {
+//      System.out.println(entry.getKey() + ": " + entry.getValue());
+//    }
+//  }
+
+  public int getNumberOfHiddenAdds() {
+    List<ElementHandle> elements = page.querySelectorAll("[data-test-item-type='standard']");
+    int count = 0;
+
+    for (ElementHandle element : elements) {
+      String attributeValue = element.getAttribute("data-test-item-state");
+      if (attributeValue.equals("hidden")) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  public int getNumberOfBlockedAdds() {
+    List<ElementHandle> elements = page.querySelectorAll("[data-test-item-type='standard']");
+    int count = 0;
+
+    for (ElementHandle element : elements) {
+      String attributeValue = element.getAttribute("data-test-item-state");
+      if (attributeValue.equals("blocked")) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  public int getNumberOfPublicAdds() {
+    List<ElementHandle> elements = page.querySelectorAll("[data-test-item-type='standard']");
+    int count = 0;
+
+    for (ElementHandle element : elements) {
+      String attributeValue = element.getAttribute("data-test-item-state");
+      if (attributeValue.equals("public")) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  public int getNumberOfNeedPayAdds() {
+    List<ElementHandle> elements = page.querySelectorAll("[data-test-item-type='standard']");
+    int count = 0;
+
+    for (ElementHandle element : elements) {
+      String attributeValue = element.getAttribute("data-test-item-state");
+      if (attributeValue.equals("need_pay")) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  public boolean checkWarningLimit(String selector) {
+    if (page.querySelector(selector).isVisible()) {
+      return true;
+    }
+    return false;
+  }
+}
---
+++ src/test/java/utils/Constants.java
@@ -0,0 +1,42 @@
+package utils;
+
+public class Constants {
+  public static final String TEXT_CAR_RUS = "Skoda Fabia 3 \n"
+      + "- 2019 г.в.\n"
+      + "- 22.800 км.\n"
+      + "- 999cm\n"
+      + "- јвтомобиль в отличном состо€нии\n"
+      + "- ѕрактически новый\n"
+      + "- 2 ключа (один новый)\n"
+      + "- ћинимальный пробег\n"
+      + "- ќдин владелец\n"
+      + "Telegram canal: https://t.me/MTS_AutoVentas\n"
+      + "Instagram:     https://www.instagram.com/p/CthLp1ZootP\n"
+      + "\n"
+      + "ѕродаю свой автомобиль, который можно назвать насто€щей находкой. Ётот автомобиль почти новый и имеет минимальный пробег, что делает его идеальным выбором дл€ тех, кто ценит качество и надежность.\n"
+      + "\n"
+      + "≈сли вы ищете автомобиль с минимальным пробегом, почти новый, в отличном состо€нии, с историей одного хоз€ина, двум€ ключам, включа€ один новый, то этот автомобиль идеально подходит дл€ вас. Ётот автомобиль станет надежным спутником на дороге и принесет вам множество при€тных моментов.\n";
+
+  public static final String  TEXT_CAR_RO = "Skoda Fabia 3 \n"
+      + "- 2019 a.f.\n"
+      + "- 22.800 km.\n"
+      + "- 999cm\n"
+      + "- Masina e in stare tehnica excelenta\n"
+      + "- Masina este practic noua\n"
+      + "- Parcurs minim\n"
+      + "- Procurata de la Daac Hermes\n"
+      + "- Sunt primul proprietar a masinei\n"
+      + "- QR din poze, sau chat 999.md";
+
+  public static final String TEXT_SSD_TITLE = "Kingston NV2 500Gb PCIe 4.0 Ч 2шт по 620/шт или 1 шт по 670";
+  //  private final String textSSDToAdd = "Nou, sigilat, original\n"
+//      + "Ќовый, запечатанный, оригинальный\n"
+//      + "Kingston NV2 500Gb, PCIe 4.0, NVMe";
+//
+  public static final String TAG_CAR = "Skoda, Fabia, Skoda Fabia, Skoda Fabia 3, нова€, новый";
+
+  public static final String TEXT_SSD_ADD = "Kingston NV2 500Gb, PCIe 4.0, NVMe\n"
+      + "=============================\n"
+      + "Ќовые, запечатанные, оригинальные";
+
+}
